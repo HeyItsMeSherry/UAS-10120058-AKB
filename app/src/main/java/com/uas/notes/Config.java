ad173592c16eab5100ea7a79953c6e3ed2637c06
Vidi Anandisa <vidiaa23@gmail.com>
2023-08-10 01:05:52 +0700

    Initial commit

---
+++ app/src/main/java/com/uas/notes/Config.java
@@ -0,0 +1,20 @@
+/**
+ * NIM : 10120058
+ * NAMA : Vidi Anandisa Fortuna
+ * KELAS : IF-2
+ */
+package com.uas.notes;
+
+public class Config {
+
+    public static String DB_URL = "https://uas-10120058-if2-default-rtdb.asia-southeast1.firebasedatabase.app/";
+    public static String FCM_SERVER_KEY = "AAAAlTKf4Do:APA91bF1Fy_urLW8P9qk-QKbpQdK7FGJp9v09Uiy1YW409cjZflHiecfOtfLyhBT4PdjuwtYYpm9nlznlPEuEdm0Y2zwBxt8aWANcq0Dw1Rde4fQNZDrzsrYyBgxIsG9pvmjDD-gdc4B";
+
+    public static String getDB_URL() {
+        return DB_URL;
+    }
+
+    public static String getFCM_SERVER_KEY() {
+        return FCM_SERVER_KEY;
+    }
+}
